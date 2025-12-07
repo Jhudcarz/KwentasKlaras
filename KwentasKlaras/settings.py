8543cf991c7a63b195ebf4d951ea5d4eec018104
jhudcarz <carzanojhudiel@gmail.com>
2025-12-07 20:58:38 +0800

    Fix ALLOWED_HOSTS for Render

--- KwentasKlaras/settings.py
+++ KwentasKlaras/settings.py
@@ -36,7 +36,9 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['kwentasklaras-2.onrender.com']
+ALLOWED_HOSTS = [ "kwentasklaras-2.onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
